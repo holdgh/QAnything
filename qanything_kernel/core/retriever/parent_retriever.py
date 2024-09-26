8bc2686ddf2805eade8306008a0c3da4fce06dd6
gaohuan <gaoh28@chinaunicom.cn>
2024-09-26 16:07:20 +0800

    保存文档至向量数据库操作梳理完成

--- qanything_kernel/core/retriever/parent_retriever.py
+++ qanything_kernel/core/retriever/parent_retriever.py
@@ -75,13 +75,16 @@ class SelfParentRetriever(ParentDocumentRetriever):
             es_store: Optional[ElasticsearchStore] = None,
             single_parent: bool = False,
     ) -> Tuple[int, Dict]:
+        """
+        对文档列表进行父切分和子切分，将切分【换行符和中文符号】后的文档列表【经过一些元数据的处理后】存入向量数据库、es和数据库文档存储
+        """
         # insert_logger.info(f"Inserting {len(documents)} complete documents, single_parent: {single_parent}")
         # 切分起始时间
         split_start = time.perf_counter()
         if self.parent_splitter is not None and not single_parent:
-            # 父切分符非空且非单亲时
+            # 父切分符非空且非单亲时【对有表格且文档内容token数量超过父切分尺寸的文档进行切分处理【依据换行符和中文标点符号】，然后将所有文档再收集到documents中】
             # documents = self.parent_splitter.split_documents(documents)
-            # 初始化切分文档列表和需要切分的文档列表
+            # 初始化已切分文档列表和待切分的文档列表
             split_documents = []
             need_split_docs = []
             # 遍历文档列表
@@ -89,53 +92,83 @@ class SelfParentRetriever(ParentDocumentRetriever):
                 if doc.metadata['has_table'] or num_tokens_embed(doc.page_content) <= parent_chunk_size:
                     # 当前文档有表格或者当前文档内容的token数量不超过父切分尺寸时
                     if need_split_docs:
-                        # 需要切分的文档列表非空时
-                        # todo 至此，待续
+                        # 待切分的文档列表非空时【将后面【当前文档没有表格且当前文档内容的token数量超过父切分尺寸时，则将当前文档收集到待切分的文档列表中】这些文档进行切分处理】
+                        # 利用分隔符【换行符和中文标点符号】对其进行切分，并收集到已切分的文档列表中
                         split_documents.extend(self.parent_splitter.split_documents(need_split_docs))
+                        # 将待切分的文档列表置为空【说明根据换行符和中文标点符号已对待切分的文档列表切分完毕】
                         need_split_docs = []
+                    # 当前文档有表格或者当前文档内容的token数量不超过父切分尺寸时，且待切分的文档列表为空，则将当前文档收集到已切分的文档列表中
                     split_documents.append(doc)
                 else:
+                    # 当前文档没有表格且当前文档内容的token数量超过父切分尺寸时，将当前文档收集到待切分的文档列表中
                     need_split_docs.append(doc)
             if need_split_docs:
+                # 待切分的文档列表非空时【将上面【当前文档没有表格且当前文档内容的token数量超过父切分尺寸时，将当前文档收集到待切分的文档列表中】这些文档进行切分处理】
+                # 利用分隔符【换行符和中文标点符号】对其进行切分，并收集到已切分文档列表中
                 split_documents.extend(self.parent_splitter.split_documents(need_split_docs))
+            # 将已切分的文档列表赋值给文档列表
             documents = split_documents
+        # 打印父切分处理情况：正在插入**个父切分文档
         insert_logger.info(f"Inserting {len(documents)} parent documents")
+
         if ids is None:
+            # ids为none时，根据insert_documents方法中ids的定义可知【当single_parent为False时，ids为None；当single_parent为True时，ids为文档id列表】，此时single_parent为False【TODO 文档列表来自于多个文件？】
+            # 提取第一个文档元数据中的文件id
             file_id = documents[0].metadata['file_id']
+            # 按照规则“文件id_文档索引”生成文档id，并收集到doc_ids中
             doc_ids = [file_id + '_' + str(i) for i, _ in enumerate(documents)]
             if not add_to_docstore:
+                # add_to_docstore默认值为True，在insert_files_server调用时，此处不会触发异常
                 raise ValueError(
                     "If ids are not passed in, `add_to_docstore` MUST be True"
                 )
         else:
+            # ids非空时【此时single_parent为True，没有进行上面的父切分，文档列表的文档数量与ids尺寸应该相等】，TODO 文档列表来自于一个文件
             if len(documents) != len(ids):
+                # 文档列表的长度与ids的长度不一致时，触发异常
                 raise ValueError(
                     "Got uneven list of documents and ids. "
                     "If `ids` is provided, should be same length as `documents`."
                 )
+            # 将ids直接赋值给doc_ids
             doc_ids = ids
-
+        # 初始化
         docs = []
         full_docs = []
+        # 遍历枚举文档列表，获取索引和相应索引的文档
         for i, doc in enumerate(documents):
+            # 对应文档索引，获取doc_ids中的对应索引的文档id
             _id = doc_ids[i]
+            # 对当前文档，进行子切分【按照换行符和中文标点符号】，得到子切分文档列表sub_docs
             sub_docs = self.child_splitter.split_documents([doc])
+            # 当前对象的child_metadata_fields属性并未设置，而child_metadata_fields默认为none
+            # 推测意为：子文档元数据属性列表
             if self.child_metadata_fields is not None:
+                # 不为none时，遍历子切分得到的文档列表
                 for _doc in sub_docs:
+                    # 设置当前文档的元数据
                     _doc.metadata = {
                         k: _doc.metadata[k] for k in self.child_metadata_fields
                     }
+            # 遍历子切分文档列表，设置文档id元数据和在文档内容中追加headers元数据
             for _doc in sub_docs:
+                # 设置子切分文档id元数据【id_key为"doc_id"】
                 _doc.metadata[self.id_key] = _id
+                # 在子切分文档内容中追加headers元数据
                 _doc.page_content = f"[headers]({_doc.metadata['headers']})\n" + _doc.page_content  # 存入page_content，向量检索时会带上headers
+            # 收集子切分文档列表结果
             docs.extend(sub_docs)
+            # 将当前文档的headers元数据追加到其内容中
             doc.page_content = f"[headers]({doc.metadata['headers']})\n" + doc.page_content  # 存入page_content，等检索后rerank时会带上headers信息
+            # 收集文档id和文档元组到列表
             full_docs.append((_id, doc))
+        # 打印日志：正在插入多少个子切分文档，并给出第一个子切分文档的元数据和前100行内容
         insert_logger.info(f"Inserting {len(docs)} child documents, metadata: {docs[0].metadata}, page_content: {docs[0].page_content[:100]}...")
+        # 记录切分时长
         time_record = {"split_time": round(time.perf_counter() - split_start, 2)}
-
+        # 执行拷贝，将子切分文档列表拷贝【赋值操作相当于不同的变量指向同一个数据，变量值和地址都是数据的值和地址，通过一个变量对数据进行操作，另一个变量的数据也同时变化。深拷贝相当于将一个变量的数据拷贝一份，放在一个新的地址上，这样两个变量的数据相同，但是地址不同，可以避免相互间的影响】给embed_docs
         embed_docs = copy.deepcopy(docs)
-        # 补充metadata信息
+        # 删除一些metadata信息
         for idx, doc in enumerate(embed_docs):
             del doc.metadata['title_lst']
             del doc.metadata['has_table']
@@ -144,22 +177,32 @@ class SelfParentRetriever(ParentDocumentRetriever):
             del doc.metadata['nos_key']
             del doc.metadata['faq_dict']
             del doc.metadata['page_id']
-
+        # 对embed_docs执行向量数据库存储【TODO 这里时长参数有什么用？】
         res = await self.vectorstore.aadd_documents(embed_docs, time_record=time_record)
+        # 打印日志，在向量数据库中的插入的数量
         insert_logger.info(f'vectorstore insert number: {len(res)}, {res[0]}')
+        # es存储操作
         if es_store is not None:
+            # es存储客户端不为none时，也即存在时
             try:
+                # es存储开始时间
                 es_start = time.perf_counter()
-                # docs的doc_id是file_id + '_' + i
+                # docs_ids中的doc_id是file_id + '_' + i
                 docs_ids = [doc.metadata['file_id'] + '_' + str(i) for i, doc in enumerate(embed_docs)]
+                # 对embed_docs执行es存储
                 es_res = await es_store.aadd_documents(embed_docs, ids=docs_ids)
+                # 记录es存储时长
                 time_record['es_insert_time'] = round(time.perf_counter() - es_start, 2)
+                # 打印日志：es存储了多少数据
                 insert_logger.info(f'es_store insert number: {len(es_res)}, {es_res[0]}')
             except Exception as e:
+                # es存储发生异常时，打印日志：es存储异常及异常信息
                 insert_logger.error(f"Error in aadd_documents on es_store: {traceback.format_exc()}")
 
         if add_to_docstore:
+            # add_to_docstore默认值为True，在insert_files_server调用时，此处对未进行子切分的文档列表进行存储【在当前方法aadd_documents的调用处，insert_documents方法中，设置了self.retriever的docstore=MysqlStore(self.mysql_client)，所以此处为数据库存储】
             await self.docstore.amset(full_docs)
+        # 返回向量数据库存储返回值和时长记录字典
         return len(res), time_record
 
 
@@ -194,13 +237,17 @@ class ParentRetriever:
     async def insert_documents(self, docs, parent_chunk_size, single_parent=False):
         """
         将文档列表插入到向量数据库
+        流程如下：
+        1、以参数中的切片尺寸【来源于文件信息中的chunk_size】为准
+        2、设置父切分符列表和子切分符列表【换行符和中文标点】，设置存储客户端【数据库、向量数据库、es】
+        3、对文档列表进行父切分和子切分，将切分【换行符和中文符号】后的文档列表【经过一些元数据的处理后】存入向量数据库、es和数据库文档存储
         """
         # 打印日志：文档列表的长度、切片尺寸
         insert_logger.info(f"Inserting {len(docs)} documents, parent_chunk_size: {parent_chunk_size}, single_parent: {single_parent}")
         if parent_chunk_size != self.parent_chunk_size:
             # 切片尺寸不等，以参数中的切片尺寸为准
             self.parent_chunk_size = parent_chunk_size
-            # 父切分符号
+            # 父切分符号【换行符和中文标点符号】
             parent_splitter = RecursiveCharacterTextSplitter(
                 separators=["\n\n", "\n", "。", "!", "！", "?", "？", "；", ";", "……", "…", "、", "，", ",", " ", ""],
                 chunk_size=parent_chunk_size,
@@ -210,7 +257,7 @@ class ParentRetriever:
                 length_function=num_tokens_embed)
             # 设置子切分尺寸，取默认子切分尺寸和当前父切分尺寸一半的最小值
             child_chunk_size = min(DEFAULT_CHILD_CHUNK_SIZE, int(parent_chunk_size / 2))
-            # 子切分符号
+            # 子切分符号【换行符和中文标点符号】
             child_splitter = RecursiveCharacterTextSplitter(
                 separators=["\n\n", "\n", "。", "!", "！", "?", "？", "；", ";", "……", "…", "、", "，", ",", " ", ""],
                 chunk_size=child_chunk_size,
@@ -229,9 +276,9 @@ class ParentRetriever:
                 parent_splitter=parent_splitter
             )
         # insert_logger.info(f'insert documents: {len(docs)}')
-        # 当single_parent为False时，ids为None；当single_parent为True时，ids为文档id列表
+        # 当single_parent为False【默认值，表示非单亲，也即文档列表来自于多个文件。TODO 在insert_files_server中，处理文件是逐个处理的，此处为什么默认值为False呢？】时，ids为None；当single_parent为True时，ids为文档id列表
         ids = None if not single_parent else [doc.metadata['doc_id'] for doc in docs]
-        # 插入文档列表，入参：文档列表、父切分尺寸、es存储、文档id列表、单亲标识
+        # 插入【对文档列表进行父切分和子切分，将切分【换行符和中文符号】后的文档列表【经过一些元数据的处理后】存入向量数据库、es和数据库文档存储】文档列表，入参：文档列表、父切分尺寸、es存储、文档id列表、单亲标识
         return await self.retriever.aadd_documents(docs, parent_chunk_size=parent_chunk_size,
                                                    es_store=self.es_store, ids=ids, single_parent=single_parent)
 
